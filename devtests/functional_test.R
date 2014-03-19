8f6c365205ef27e80d1ede942579594cdc190030
Camille Marchet <marchetcamille@gmail.com>
2014-03-19 16:54:11 +0000

    Updated the functional tests : working with two types of input, data.frame from pretraitement.txt or output (.fa) from kissplice. Output from Kissplice file added is equivalent to pretraitement.txt in term of results.

--- devtests/functional_test.R
+++ devtests/functional_test.R
@@ -1,10 +1,23 @@
 install.packages("kissDE_1.0.tar.gz")
 library("kissDE")
-data<-("pretraitement.txt")
+
+
+#### test for input = data.frame ####
+data<-read.table("pretraitement.txt")
 # signifEventsRef <-diffExpressedEvents(2,c(2,2),data)
-signifEvents<-diffExpressedEvents(2,c(2,2),data)
+signifEventsDF<-diffExpressedEvents(2,c(2,2),data)
 
 # save(signifEventsRef,file="signifEventsRef.RData")
 
 load("signifEventsRef.RData")
-signifEventsRef==signifEvents
+signifEventsRef==signifEventsDF
+
+
+#### test for input = .fa file from KisSplice ####
+# signifEventsRef <-diffExpressedEvents(2,c(2,2),data)
+signifEventsKis<-diffExpressedEvents(2,c(2,2),"resultsKissSknsh10M.fa")
+
+# save(signifEventsRef,file="signifEventsRef.RData")
+
+load("signifEventsRef.RData")
+signifEventsRef==signifEventsKis
