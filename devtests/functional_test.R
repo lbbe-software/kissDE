99dc7752359ad8fa45368e28b855354e74e2bf9b
Camille Marchet <marchetcamille@gmail.com>
2014-03-27 15:16:11 +0000

    functional test modified again as a consequence of last changes in diffExpressedEvents.R

--- devtests/functional_test.R
+++ devtests/functional_test.R
@@ -2,17 +2,17 @@ install.packages("../../kissDE_1.0.tar.gz")
 library("kissDE")
 
 conditions <- c("C1","C1","C2","C2")
-
+file <- ("resultsKissSknsh10M.fa")
 #### test for input = data.frame ####
 # counts <- read.table("pretraitement.txt")
 # colnames(counts) = c("ID","length",conditions)
 
 #### test for input = .fa file from KisSplice ####
 # signifEventsRef <-diffExpressedEvents(2,c(2,2),data)
-counts <- kissplice2counts("resultsKissSknsh10M.fa",conditions)
+counts <- kissplice2counts(file)
 
-qualityControl(counts)
-signifEventsKis<-diffExpressedEvents(counts)
+qualityControl(counts,conditions)
+signifEventsKis<-diffExpressedEvents(counts,conditions)
 
 # save(signifEventsRef,file="signifEventsRef.RData")
 load("signifEventsRef.RData")
