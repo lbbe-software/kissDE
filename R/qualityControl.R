64ff512daaf51ede2ae341b87aed0b8c6a1189bd
Aurélie Siberchicot <aurelie.siberchicot@univ-lyon1.fr>
2017-03-01 10:46:30 +0000

    Trunc lines after 80 characters and comments now begin with two # symbol, in the qualityControl function

--- R/qualityControl.R
+++ R/qualityControl.R
@@ -1,6 +1,6 @@
 qualityControl <- function(countsData, conditions, storeFigs=FALSE) {
 	
-	options(warn=-1)  # suppress the warning for the users
+	options(warn=-1)  ## suppress the warning for the users
 	
 	if (storeFigs == FALSE) {
 		pathToFigs <- NA
@@ -12,7 +12,7 @@ qualityControl <- function(countsData, conditions, storeFigs=FALSE) {
 		}
 	}
 	
-	# create a new folder if it doesn't exist
+	## create a new folder if it doesn't exist
 	if (!is.na(pathToFigs)) {
 		find <- paste("find", pathToFigs)
 		d <- system(find, TRUE, ignore.stderr=TRUE)
@@ -35,36 +35,44 @@ qualityControl <- function(countsData, conditions, storeFigs=FALSE) {
 	###################################################
 	### select events with highest variance (on PSI)
 	###################################################
-	countsData2 <- reshape(countsData[, c(1,(dimns):(dimns + length(conds)))], timevar="Path", idvar="ID", direction="wide")
+	countsData2 <- reshape(countsData[, c(1,(dimns):(dimns + length(conds)))], 
+												 timevar="Path", idvar="ID", direction="wide")
 	for (i in 1:n){
 		for (j in 1:nr[i]){
-			countsData2$PSI <- countsData2[,(1+j+sum(nr[0:(i-1)]))]/
-				(countsData2[,(1+j+sum(nr[0:(i-1)]))] + countsData2[,(1+sum(nr)+j+sum(nr[0:(i-1)]))])
-			# replace PSI by NA if count less or equal to 10 reads for the 2 isoforms
-			indexNA <- intersect(which(countsData2[,(1+j+sum(nr[0:(i-1)]))] < 10), which(countsData2[,(1+sum(nr)+j+sum(nr[0:(i-1)]))] < 10))
+			countsData2$PSI <- countsData2[, (1+j+sum(nr[0:(i-1)]))] /
+				(countsData2[, (1+j+sum(nr[0:(i-1)]))] + 
+				 	countsData2[, (1+sum(nr)+j+sum(nr[0:(i-1)]))])
+			## replace PSI by NA if count less or equal to 10 reads for the 2 isoforms
+			indexNA <- intersect(which(countsData2[, (1+j+sum(nr[0:(i-1)]))] < 10), 
+										which(countsData2[, (1+sum(nr)+j+sum(nr[0:(i-1)]))] < 10))
 			countsData2$PSI[indexNA] <- NA
-			colnames(countsData2)[9+j+(i-1)*2] <- paste(paste0("Cond",i),paste0("repl",j),sep="_")
+			colnames(countsData2)[9+j+(i-1)*2] <- paste(paste0("Cond", i),
+																									paste0("repl", j), sep="_")
 		}
 	}
-	countsData2$vars <- apply(as.matrix(countsData2[, 10:(9 + length(conds))]), 1, var, na.rm=TRUE)
+	countsData2$vars <- apply(as.matrix(countsData2[, 10:(9 + length(conds))]), 
+														1, var, na.rm=TRUE)
 	ntop <- min(500, dim(countsData2)[1])
 	selectntop <- order(countsData2$vars, decreasing=TRUE)[seq_len(ntop)]
 	countsData2Selected <- countsData2[selectntop,]
 	# remove all NAs
-	countsData2Selected <- countsData2Selected[complete.cases(countsData2Selected[, 10:(9 + length(conds))]),]
+	countsData2Selected <- countsData2Selected[complete.cases(
+		countsData2Selected[, 10:(9 + length(conds))]), ]
 	
 	###################################################
 	### code chunk number 3: heatmap
 	###################################################
 	if (storeFigs == FALSE) {
-		heatmap.2(as.matrix(as.dist(1 - cor(countsData2Selected[, 10:(9 + length(conds))]))), margins=c(10, 10), 
-							cexRow=1, cexCol=1, density.info="none", trace="none")
+		heatmap.2(as.matrix(as.dist(1 - 
+			cor(countsData2Selected[, 10:(9 + length(conds))]))), margins=c(10, 10), 
+			cexRow=1, cexCol=1, density.info="none", trace="none")
 		par(ask=TRUE)
 	} else {
 		filename <- paste(storeFigs, "/heatmap.png", sep="")
 		png(filename)
-		heatmap.2(as.matrix(as.dist(1 - cor(countsData2Selected[, 10:(9 + length(conds))]))), margins=c(10, 10), 
-							cexRow=1, cexCol=1, density.info="none", trace="none")
+		heatmap.2(as.matrix(as.dist(1 - 
+			cor(countsData2Selected[, 10:(9 + length(conds))]))), margins=c(10, 10), 
+			cexRow=1, cexCol=1, density.info="none", trace="none")
 		void <- dev.off()
 	}
 	
@@ -73,7 +81,8 @@ qualityControl <- function(countsData, conditions, storeFigs=FALSE) {
 	###################################################
 	pca <- prcomp(t(countsData2Selected[, 10:(9 + length(conds))]))
 	fac <- factor(conds)
-	colorpalette <- c("#192823", "#DD1E2F", "#EBB035", "#06A2CB", "#218559", "#D0C6B1")
+	colorpalette <- c("#192823", "#DD1E2F", "#EBB035", "#06A2CB", 
+										"#218559", "#D0C6B1")
 	colors <- colorpalette[1:n]
 	pc1var <- round(summary(pca)$importance[2,1]*100, digits=1)
 	pc2var <- round(summary(pca)$importance[2,2]*100, digits=1)
@@ -81,18 +90,22 @@ qualityControl <- function(countsData, conditions, storeFigs=FALSE) {
 	pc2lab <- paste0("PC2 (",as.character(pc2var),"%)")
 	if (storeFigs == FALSE) {
 		par(oma=c(2, 1, 1, 1))
-		plot(PC2~PC1, data=as.data.frame(pca$x), bg=colors[fac], pch=21, xlab=pc1lab, ylab=pc2lab, main="PCA plot")
+		plot(PC2~PC1, data=as.data.frame(pca$x), bg=colors[fac], pch=21, 
+				 xlab=pc1lab, ylab=pc2lab, main="PCA plot")
 		par(fig=c(0, 1, 0, 1), oma=c(0, 0, 0, 0), mar=c(0, 0, 0, 0), new=TRUE)
 		plot(0, 0, type="n", bty="n", xaxt="n", yaxt="n")
-		legend("bottom", legend=levels(fac), xpd=TRUE, horiz=TRUE, inset=c(0, 0), bty="n", pch=20, col=colors)
+		legend("bottom", legend=levels(fac), xpd=TRUE, horiz=TRUE, inset=c(0, 0), 
+					 bty="n", pch=20, col=colors)
 	} else {
 		filename <- paste(storeFigs, "/pca.png", sep="")
 		png(filename)
 		par(oma=c(2, 1, 1, 1))
-		plot(PC2~PC1, data=as.data.frame(pca$x), bg=colors[fac], pch=21, xlab=pc1lab, ylab=pc2lab, main="PCA plot")
+		plot(PC2~PC1, data=as.data.frame(pca$x), bg=colors[fac], pch=21, 
+				 xlab=pc1lab, ylab=pc2lab, main="PCA plot")
 		par(fig=c(0, 1, 0, 1), oma=c(0, 0, 0, 0), mar=c(0, 0, 0, 0), new=TRUE)
 		plot(0, 0, type="n", bty="n", xaxt="n", yaxt="n")
-		legend("bottom", legend=levels(fac), xpd=TRUE, horiz=TRUE, inset=c(0, 0), bty="n", pch=20, col=colors)
+		legend("bottom", legend=levels(fac), xpd=TRUE, horiz=TRUE, inset=c(0, 0), 
+					 bty="n", pch=20, col=colors)
 		void <- dev.off()
 	}
 	
