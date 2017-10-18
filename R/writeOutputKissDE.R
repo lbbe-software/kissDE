91cfc5c070936895f444f115eec789245e597a4c
audric.cologne <audric.cologne@gmail.com>
2017-10-18 13:45:06 +0200

    Input verifications : adjPvalMax and dPSImin.

--- R/writeOutputKissDE.R
+++ R/writeOutputKissDE.R
@@ -1,11 +1,11 @@
-writeOutputKissDE <- function(resDiffExprVariant, adjPvalMax=1, dPSImin=0, 
-															output, writePSI=FALSE) {
-	if (adjPvalMax > 1 || adjPvalMax < 0) {
-		stop("Invalid pvalMax (0 <= pvalMax <= 1).")
+writeOutputKissDE <- function(resDiffExprVariant, output, adjPvalMax=1, 
+                              dPSImin=0, writePSI=FALSE) {
+	if (length(adjPvalMax) > 1 | !is.double(adjPvalMax) | adjPvalMax < 0 | adjPvalMax > 1) {
+		stop("Input error : adjPvalMax option must be a double between 0 and 1.")
 	}
 	
-	if (dPSImin > 1 || dPSImin < 0) {
-		stop("Invalid dPSImin (0 <= dPSImin <= 1). A dPSImin = 0.1 
+	if (length(dPSImin) > 1 | !is.double(dPSImin) | dPSImin < 0 | dPSImin > 1) {
+		stop("Input error : dPSImin option must be a double between 0 and 1. A dPSImin = 0.1 
             will catch all dPSI from -1 to -0.1 and all dPSI from 0.1 to 1.")
 	}
 	
