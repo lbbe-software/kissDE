311bfc6166f87da9cec18ba6ac0935989956cad0
cbenoitp <clara.benoitp@gmail.com>
2018-07-13 15:13:21 +0200

    Add new tests for the nbCore and pvalue parameters in diffExpressedVariants

--- tests/testthat/test_diffExpressedVariants_counts.R
+++ tests/testthat/test_diffExpressedVariants_counts.R
@@ -15,4 +15,37 @@ test_that("diffExpressedVariants function works as expected on count file", {
   expect_equal(dim(diff$finalTable[which(diff$finalTable$Adjusted_pvalue <= 0.05),])[1], 19)
   expect_equal(diff$finalTable[which(diff$finalTable$ID == "event1"), "Deltaf/DeltaPSI"], -0.7824)
 })
-  
+
+test_that("diffExpressedVariants function works as expected on count file with 2 cores", {
+  # test nbCore parameter
+  fpath1 <- system.file("extdata", "table_counts_alt_splicing.txt", package="kissDE")
+  tableCounts <- read.table(fpath1, head = TRUE)
+  conditions <- c("C1", "C1", "C2", "C2")
+  if (detectCores() >= 2){
+    diff <- diffExpressedVariants(tableCounts, conditions, nbCore=2)
+    expect_equal(names(diff), c("finalTable", "correctedPVal", "uncorrectedPVal", "resultFitNBglmModel", "f/psiTable", "k2rgFile"))
+    expect_equal(dim(diff$finalTable)[2], 13)
+    expect_equal(dim(diff$finalTable)[1], length(diff$correctedPVal))
+    expect_equal(dim(diff$finalTable)[1], dim(diff$`f/psiTable`)[1])
+    expect_equal(names(diff$correctedPVal), names(diff$uncorrectedPVal))
+    expect_equal(names(diff$correctedPVal), rownames(diff$resultFitNBglmModel))
+    expect_null(diff$k2rgFile)
+    expect_equal(dim(diff$finalTable[which(diff$finalTable$Adjusted_pvalue <= 0.05),])[1], 19)
+    expect_equal(diff$finalTable[which(diff$finalTable$ID == "event1"), "Deltaf/DeltaPSI"], -0.7824)
+  }
+})
+
+test_that("pvalue parameter of diffExpressedVariants function works as expected", {
+  # test pvalue parameter
+  fpath1 <- system.file("extdata", "table_counts_alt_splicing.txt", package="kissDE")
+  tableCounts <- read.table(fpath1, head = TRUE)
+  conditions <- c("C1", "C1", "C2", "C2")
+  diff <- diffExpressedVariants(tableCounts, conditions, pvalue = 0.05)
+  expect_equal(names(diff), c("finalTable", "correctedPVal", "uncorrectedPVal", "resultFitNBglmModel", "f/psiTable", "k2rgFile"))
+  expect_equal(dim(diff$finalTable)[2], 13)
+  expect_equal(dim(diff$finalTable)[1], 19)
+  expect_equal(names(diff$correctedPVal), names(diff$uncorrectedPVal))
+  expect_equal(names(diff$correctedPVal), rownames(diff$resultFitNBglmModel))
+  expect_null(diff$k2rgFile)
+  expect_equal(diff$finalTable[which(diff$finalTable$ID == "event1"), "Deltaf/DeltaPSI"], -0.7824)
+})
