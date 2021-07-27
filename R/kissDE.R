6b6e6d1c10e1f78dbb3dcc98caf3fdf318314dc4
Aurélie Siberchicot <aurelie.siberchicot@univ-lyon1.fr>
2021-07-27 11:01:03 +0200

    Clean and correct some misprints in the new 'kissDE' function

--- R/kissDE.R
+++ R/kissDE.R
@@ -1,8 +1,10 @@
-kissDE <- function(fileName, conditions, output, counts=2, pairedEnd=FALSE, order=NULL, exonicReads=TRUE, k2rg=FALSE, keep=c("All"), remove=NULL, pvalue=1, filterLowCountsVariants=10, flagLowCountsConditions=10, technicalReplicates=FALSE, nbCore=1, adjPvalMax=1, dPSImin=0, writePSI=FALSE) {
-  count=kissplice2counts(fileName, counts, pairedEnd, order, exonicReads, k2rg, keep, remove)
-  res=diffExpressedVariants(count,conditions, pvalue, filterLowCountsVariants, flagLowCountsConditions, technicalReplicates, nbCore)
-  writeOutputKissDE(resDiffExprVariant, output, adjPvalMax, dPSImin)
+kissDE <- function(fileName, conditions, output, counts = 2, pairedEnd = FALSE, order = NULL, exonicReads = TRUE, k2rg = FALSE, keep = c("All"), remove = NULL, pvalue = 1, 
+                   filterLowCountsVariants = 10, flagLowCountsConditions = 10, technicalReplicates = FALSE, nbCore = 1, adjPvalMax = 1, dPSImin = 0, writePSI = FALSE) {
+  count <- kissplice2counts(fileName, counts, pairedEnd, order, exonicReads, k2rg, keep, remove)
+  res <- diffExpressedVariants(count, conditions, pvalue, filterLowCountsVariants, flagLowCountsConditions, technicalReplicates, nbCore)
+  
+  writeOutputKissDE(res, output, adjPvalMax, dPSImin)
   if(writePSI) {
-    writeOutputKissDE(resDiffExprVariant, paste(output,"PSIs",sep="."), adjPvalMax, dPSImin, writePSI)
+    writeOutputKissDE(res, paste(output, "PSIs", sep = "."), adjPvalMax, dPSImin, writePSI)
   }
 }
