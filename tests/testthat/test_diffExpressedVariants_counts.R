8f340864426f62a8a97c1f02379d84f874f5ecaa
dwishsan <audric.cologne@gmail.com>
2021-08-18 14:29:15 +0200

    Update test_diffExpressedVariants_counts.R
    Trying to resolve error on Windows with testthat

--- tests/testthat/test_diffExpressedVariants_counts.R
+++ tests/testthat/test_diffExpressedVariants_counts.R
@@ -1,7 +1,7 @@
 context("diffExpressedVariants_count")
 test_that("diffExpressedVariants function works as expected on count file", {
   # test on count file
-  fpath1 <- system.file("extdata", "table_counts_alt_splicing.txt", package="kissDE")
+  fpath1 <- system.file("extdata/table_counts_alt_splicing.txt", package="kissDE", mustWork=TRUE)
   tableCounts <- read.table(fpath1, head = TRUE)
   conditions <- c("C1", "C1", "C2", "C2")
   diff <- diffExpressedVariants(tableCounts, conditions)
@@ -21,7 +21,7 @@ test_that("diffExpressedVariants function works as expected on count file with 2
   skip_on_bioc()
   # test nbCore parameter
   if (detectCores() - 1 >= 2){
-    fpath1 <- system.file("extdata", "table_counts_alt_splicing.txt", package="kissDE")
+    fpath1 <- system.file("extdata/table_counts_alt_splicing.txt", package="kissDE", mustWork=TRUE)
     tableCounts <- read.table(fpath1, head = TRUE)
     conditions <- c("C1", "C1", "C2", "C2")
     diff <- diffExpressedVariants(tableCounts, conditions, nbCore = 2)
@@ -39,7 +39,7 @@ test_that("diffExpressedVariants function works as expected on count file with 2
 
 test_that("pvalue parameter of diffExpressedVariants function works as expected", {
   # test pvalue parameter
-  fpath1 <- system.file("extdata", "table_counts_alt_splicing.txt", package="kissDE")
+  fpath1 <- system.file("extdata/table_counts_alt_splicing.txt", package="kissDE", mustWork=TRUE)
   tableCounts <- read.table(fpath1, head = TRUE)
   conditions <- c("C1", "C1", "C2", "C2")
   diff <- diffExpressedVariants(tableCounts, conditions, pvalue = 0.05)
