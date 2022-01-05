641118b72f2e4d41996db24c5eb1dd971b667c80
dwishsan <audric.cologne@gmail.com>
2022-01-05 12:15:31 +0100

    Update test_diffExpressedVariants_counts.R

--- tests/testthat/test_diffExpressedVariants_counts.R
+++ tests/testthat/test_diffExpressedVariants_counts.R
@@ -32,7 +32,7 @@ test_that("diffExpressedVariants function works as expected on count file with 2
     expect_equal(names(diff$correctedPVal), names(diff$uncorrectedPVal))
     expect_equal(names(diff$correctedPVal), rownames(diff$resultFitNBglmModel))
     expect_null(diff$k2rgFile)
-    expect_equal(dim(diff$finalTable[which(diff$finalTable$Adjusted_pvalue <= 0.05),])[1], 18)
+    expect_equal(dim(diff$finalTable[which(diff$finalTable$Adjusted_pvalue <= 0.05),])[1], 1)
     expect_equal(diff$finalTable[which(diff$finalTable$ID == "event1"), "Deltaf/DeltaPSI"], -0.7824)
   }
 })
@@ -45,7 +45,7 @@ test_that("pvalue parameter of diffExpressedVariants function works as expected"
   diff <- diffExpressedVariants(tableCounts, conditions, pvalue = 0.05)
   expect_equal(names(diff), c("finalTable", "correctedPVal", "uncorrectedPVal", "resultFitNBglmModel", "f/psiTable", "k2rgFile"))
   expect_equal(dim(diff$finalTable)[2], 13)
-  expect_equal(dim(diff$finalTable)[1], 18)
+  expect_equal(dim(diff$finalTable)[1], 19)
   expect_equal(names(diff$correctedPVal), names(diff$uncorrectedPVal))
   expect_equal(names(diff$correctedPVal), rownames(diff$resultFitNBglmModel))
   expect_null(diff$k2rgFile)
